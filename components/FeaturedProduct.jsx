c5c64fa25549561df8ea169665af7b69fe8e782e
SayVathanak <saksovathanaksay@gmail.com>
2025-03-27 21:37:41 +0700

    first commit

---
+++ components/FeaturedProduct.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
+const products = [
+  {
+    id: 1,
+    image: assets.girl_with_headphone_image,
+    title: "Radiate Confidence",
+    description: "Luxurious skincare for a naturally radiant glow.",
+  },
+  {
+    id: 2,
+    image: assets.girl_with_earphone_image,
+    title: "Embrace Your Glow",
+    description: "Nourish and hydrate for soft, luminous skin.",
+  },
+  {
+    id: 3,
+    image: assets.boy_with_laptop_image,
+    title: "Glow from Within",
+    description: "Rejuvenate your skin with deep hydration and care.",
+  },
+];
+
+const FeaturedProduct = () => {
+  return (
+    <div className="mt-14">
+      <div className="flex flex-col items-center">
+        <p className="text-3xl font-medium text-[#706D54]">Featured Products</p>
+        <div className="w-28 h-0.5 bg-[#A08963] mt-2"></div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14 mt-12 md:px-14 px-4">
+        {products.map(({ id, image, title, description }) => (
+          <div key={id} className="relative group">
+            <Image
+              src={image}
+              alt={title}
+              className="group-hover:brightness-90 transition duration-300 w-full h-auto object-cover"
+            />
+            <div className="group-hover:-translate-y-4 transition duration-300 absolute bottom-8 left-8 text-white space-y-2">
+              <p className="font-medium text-xl lg:text-2xl">{title}</p>
+              <p className="text-sm lg:text-base leading-5 max-w-60">
+                {description}
+              </p>
+              <button className="flex items-center gap-1.5 bg-white/10 px-4 py-2 rounded">
+                Buy now <Image className="h-3 w-3" src={assets.redirect_icon} alt="Redirect Icon" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProduct;
