37f8f427c75babcd9f1a9cf857927f4acbe064f6
sayVathanak <128988192+SayVathanak@users.noreply.github.com>
2025-05-31 18:38:17 +0700

    Update page.jsx

--- app/all-products/page.jsx
+++ app/all-products/page.jsx
@@ -323,7 +323,7 @@
 "use client";
 import { useState, useEffect, useCallback } from "react";
 import { useAppContext } from "@/context/AppContext";
-
+import { CiFilter } from "react-icons/ci";
 // Components
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer";
@@ -521,11 +521,8 @@ const AllProducts = () => {
                             }`}
                     >
                         <div className="flex justify-between items-center mb-6 lg:hidden">
-                            <h2 className="text-lg font-medium">Filters</h2>
-                            <button onClick={() => setIsSidebarOpen(false)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                </svg>
+                            <button onClick={() => setIsSidebarOpen(true)} className="text-lg font-medium">
+                                <CiFilter className="h-6 w-6" />
                             </button>
                         </div>
 
