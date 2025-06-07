13f168de99ff05782122a9213999e42fbc07fbbb
sayVathanak <saksovathanaksay@gmail.com>
2025-06-08 02:26:15 +0700

    add cdn for quaggaJS

--- app/layout.js
+++ app/layout.js
@@ -90,6 +90,7 @@ export default function RootLayout({ children }) {
               }
             `
           }} />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
         </head>
         <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
