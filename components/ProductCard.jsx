0a4c7fb5c5b1af568204602db63b4fd20d5138d2
sayVathanak <128988192+SayVathanak@users.noreply.github.com>
2025-06-13 18:55:48 +0700

    Update ProductCard.jsx

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -138,7 +138,7 @@ const ProductCard = ({ product }) => {
                             </div>
                             <button
                                 disabled
-                                className="px-2 py-2 text-gray-400 bg-gray-100 rounded-xl text-sm font-medium cursor-not-allowed"
+                                className="px-2 py-2 text-gray-400 text-sm font-medium cursor-not-allowed"
                             >
                                 Unavailable
                             </button>
