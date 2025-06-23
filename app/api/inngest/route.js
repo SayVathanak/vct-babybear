3b946a5bf33ac1ad9bf10ad126da267f995bb069
sayVathanak <saksovathanaksay@gmail.com>
2025-06-24 00:38:08 +0700

    function ims

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,27 @@
 import { serve } from "inngest/next";
-import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation, handleItemStatusUpdated, handleOrderStatusUpdated, verifyBakongPayments, checkFastApiHealth } from "@/config/inngest";
+import { 
+    inngest,
+    createUserOrder, 
+    deductInventory,         
+    restoreInventory,        
+    syncUserCreation, 
+    syncUserDeletion, 
+    syncUserUpdation, 
+    handleItemStatusUpdated, 
+    handleOrderStatusUpdated, 
+    verifyBakongPayments, 
+    checkFastApiHealth 
+} from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
+    createUserOrder,
+    deductInventory,         
+    restoreInventory,        
     syncUserCreation,
     syncUserUpdation,
     syncUserDeletion,
-    createUserOrder,
     handleItemStatusUpdated,
     handleOrderStatusUpdated,
     verifyBakongPayments,
